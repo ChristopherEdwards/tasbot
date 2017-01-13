625a91adabc0150d14fce1574808374e1e7bb080
pjgat09 <pjgat09@gmail.com>
2017-01-13 00:57:02 -0500

    Recover script is now silent and should be more reliable.

--- Scripts/recover.py
+++ Scripts/recover.py
@@ -3,11 +3,11 @@ import serial, sys, time, os, bz2, gc
 # connect to device
 ser = serial.Serial(sys.argv[1], 2000000, timeout=0.1)
 
-for i in range(0,27):
+for i in range(0,30):
   data = []
   for j in range(0,64):
     data = data + [0x00]
 
-  print(data)
+  #print(data)
 
   ser.write(bytes(data))
